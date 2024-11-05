66c67232e0a027b856ddf6c3caac57c16e5e3d5f
hugobarbosa <125314621+hugosbarbosa@users.noreply.github.com>
2024-11-04 22:13:34 -0300

    Exercico feito

--- Program.cs
+++ Program.cs
@@ -10,6 +10,51 @@ namespace CalculadorDeIMC
     {
         static void Main(string[] args)
         {
+            Console.Write("Informe seu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Informe sua altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            double imc = peso / (altura * altura);
+
+            Console.Write("Seu IMC é: " + imc.ToString("F2") + " ");
+
+            if (imc <= 18.5)
+            {
+                Console.WriteLine("Você está abaixo do peso");
+            }
+            else if (imc >= 18.6 && imc <= 25)
+            {
+                Console.WriteLine("Você está no peso ideal");
+            }
+            else if (imc > 25 && imc <= 30)
+            {
+                Console.WriteLine("Você está sobrepeso");
+            }
+
+            else if (imc > 30 && imc <= 35)
+            {
+                Console.WriteLine("Você está com obesidade grau 1");
+            }
+
+            else if (imc > 35 && imc <= 40)
+            {
+                Console.WriteLine("Você está com obesidade grau 2");
+            }
+
+            else
+            {
+                Console.WriteLine("Você está com obesidade grau 3");        
+            }
+
+            
+            Console.ReadKey();
         }
+
     }
 }
